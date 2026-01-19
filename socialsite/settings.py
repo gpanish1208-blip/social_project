84f9af32cf816cca9a60eae5c9dec9536b1a37ba
gpanish1208-blip <gp.anish.1208@gmail.com>
2026-01-19 11:41:13 +0530

    Use PostgreSQL on Render

--- socialsite/settings.py
+++ socialsite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -65,10 +65,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'socialsite.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = []
